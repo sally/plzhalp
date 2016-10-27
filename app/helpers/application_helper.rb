8cee1df8f6c6c87949d8c375af21db051777950b
Christopher Chan <christopherchan35@gmail.com>
2016-10-27 10:31:05 -0700

    Add login form partial on home page. Center login form and add border

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  # helper_method :resource_name, :resource, :devise_mapping
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
