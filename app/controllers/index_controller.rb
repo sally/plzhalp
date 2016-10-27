befcddb52d120ee5f4ffa90887a9900ed956091d
Christopher Chan <christopherchan35@gmail.com>
2016-10-27 14:54:35 -0700

    Implement upcoming appointments for both students and mentors

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -3,6 +3,16 @@ class IndexController < ApplicationController
   # index page doesnt need header partial
   def index
     @appointments = Appointment.all
+    # p current_user
+    if current_user
+      if current_user.role == 'mentor'
+        @mentor = Mentor.find_by(user_id: current_user.id)
+        @upcoming_appointments = @mentor.appointments
+      elsif current_user.role == 'student'
+        @student = Student.find_by(user_id: current_user.id)
+        @upcoming_appointments = @student.appointments
+      end
+    end
     render 'index/index'
   end
 
