76a90a18e620aef3e07adf30da271eadfbde8773
Christopher Chan <christopherchan35@gmail.com>
2016-10-27 18:45:23 -0700

    Mentor able to create appointments

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,23 +4,24 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    if current_user
-      if current_user.role == "mentor"
-       @appointment =   Appointment.new(appointment_params)
-        @mentor = Mentor.find_by(user_id: current_user.id)
-        @appointment.mentor_id = current_user.id
-          @mentor.appointments << @appointment
-      end
-      if @appointment.save
-        redirect_to '/'
-      else
-        render 'new'
-      end
+    if current_user.role == "mentor"
+      # Make a new appointment using the params
+     @appointment = Appointment.new(appointment_params)
+      # Find the mentor that is currently logged in
+     @mentor = Mentor.find_by(user_id: current_user.id)
+     @appointment.mentor_id = @mentor.id #current_user.id
+    #  @mentor.appointments << @appointment
+    end
+    if @appointment.save
+      redirect_to '/'
+    else
+      @errors = @appointment.errors.full_messages
+      render 'new'
     end
   end
 
   private
   def appointment_params
-    params.require(:appointment).permit(:mentor_id, :student_id, :date, :start_time, :end_time)
+    params.require(:appointment).permit(:mentor_id, :date, :start_time, :end_time)
   end
 end
--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -7,7 +7,7 @@ class IndexController < ApplicationController
     if current_user
       if current_user.role == 'mentor'
         @mentor = Mentor.find_by(user_id: current_user.id)
-        @upcoming_appointments = @mentor.appointments
+        @upcoming_appointments = @mentor.appointments.where.not(student_id: nil)
       elsif current_user.role == 'student'
         @student = Student.find_by(user_id: current_user.id)
         @upcoming_appointments = @student.appointments
