3eb666f8e92cc121b69d1e9dc9b51b5ae830a3e8
Young Ju Park <parkyngj@gmail.com>
2016-10-27 19:36:56 -0700

    Add logic for creating a StudentFeedback object

--- app/controllers/student_feedbacks_controller.rb
+++ app/controllers/student_feedbacks_controller.rb
@@ -4,10 +4,21 @@ class StudentFeedbacksController < ApplicationController
   end
 
   def create
-
+    @student_feedback = StudentFeedback.new(student_feedback_params)
+    @student_feedback.appointment = Appointment.find(params[:appointment_id])
+    if @student_feedback.save
+      redirect_to user_path(current_user)
+    else
+      'idgaf'
+    end
   end
 
   def show
 
+  end
+
+  private
+  def student_feedback_params
+    params.require(:student_feedback).permit(:body, :appointment_id)
   end
 end
