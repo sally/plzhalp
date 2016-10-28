1b113e6d1fe540cdf868fedbba64e3fbf35fe7ac
Christopher Chan <christopherchan35@gmail.com>
2016-10-27 17:46:20 -0700

    Change apponitments migration file for mentor and student from references to integer

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,6 +4,23 @@ class AppointmentsController < ApplicationController
   end
 
   def create
-    @appointment = Appointment.find()
+    if current_user
+      if current_user.role == "mentor"
+       @appointment =   Appointment.new(appointment_params)
+        @mentor = Mentor.find_by(user_id: current_user.id)
+        @appointment.mentor_id = current_user.id
+          @mentor.appointments << @appointment
+      end
+      if @appointment.save
+        redirect_to '/'
+      else
+        render 'new'
+      end
+    end
+  end
+
+  private
+  def appointment_params
+    params.require(:appointment).permit(:mentor_id, :student_id, :date, :start_time, :end_time)
   end
 end
